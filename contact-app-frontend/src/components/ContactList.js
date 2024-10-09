ed4081fa73a0c5f61da5694e42599da6eaacf485
phaniprat <phaneendra.kumar@pratishthanventures.com>
2024-10-09 18:15:05 +0530

    contact-app-frontend

--- contact-app-frontend/src/components/ContactList.js
+++ contact-app-frontend/src/components/ContactList.js
@@ -5,21 +5,29 @@ import './contactList.css';
 
 const ContactList = () => {
   const [contacts, setContacts] = useState([]);
-
+  const [searchQuery, setSearchQuery] = useState('');
   const handleLogout = () => {
     localStorage.removeItem('userToken');
     window.location.href = '/login';
   };
 
   useEffect(() => {
-    axios.get('http://localhost:3000/api/contacts', {
+    fetchContacts();
+  }, []);
+
+  const fetchContacts = (query) => {
+    const token = localStorage.getItem('userToken');
+    const url = query ? `http://localhost:3000/api/contacts/filter/search?name=${query}` : 'http://localhost:3000/api/contacts';
+  
+    axios.get(url, {
       headers: {
-        'Authorization': `Bearer ${localStorage.getItem('userToken')}`
+        'Authorization': `Bearer ${token}`
       }
     })
     .then(response => setContacts(response.data))
     .catch(error => console.error('There was an error fetching the contacts!', error));
-  }, []);
+  };
+  
 
   const handleSort = () => {
     axios.get('http://localhost:3000/api/contacts/sort', { 
@@ -40,25 +48,38 @@ const ContactList = () => {
       }
     })
     .then(() => {
-   console.log("Added to Favourite...")
+      console.log("Added to Favourite...");
     })
     .catch(error => console.error('There was an error updating the favorite status!', error));
+  };
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    fetchContacts(searchQuery);
   };
 
   return (
     <div>
       <h1>Contact List</h1>
       <Link to="/create">Create New Contact</Link>
+
+      <form onSubmit={handleSearch}>
+        <input 
+          type="text" 
+          placeholder="Search contact by name..." 
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)} 
+        />
+        <button type="submit">Search</button>
+      </form>
+
       <ul>
         {contacts.map(contact => (
           <li key={contact._id}>
             <Link to={`/contact/${contact._id}`}>{contact.name}</Link>
             <br />
 
-              <button 
-                 onClick={ ()=>handleFavorite(contact._id)} 
-              > Add  Favorite</button>
-
+            <button onClick={() => handleFavorite(contact._id)}>Add Favorite</button>
             <br />
             <br />
           </li>
